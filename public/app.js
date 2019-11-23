0ea47f970e84a5d91b41694bd14f5f839cb25646
Toney Higgins <toneyhiggins2@gmail.com>
2019-11-23 09:57:55 -0500

    adding models

--- public/app.js
+++ public/app.js
@@ -1,130 +1,72 @@
-// Loads results onto the page
-function getResults() {
-    // Empty any results currently on the page
-    $("#results").empty();
-    // Grab all of the current notes
-    $.getJSON("/all", function(data) {
-      // For each note...
-      for (var i = 0; i < data.length; i++) {
-        // ...populate #results with a p-tag that includes the note's title and object id
-        $("#results").prepend("<p class='data-entry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
-          data[i]._id + ">" + data[i].title + "</span><span class=delete>X</span></p>");
-      }
-    });
-  }
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+  });
   
-  // Runs the getResults function as soon as the script is executed
-  getResults();
   
-  // When the #make-new button is clicked
-  $(document).on("click", "#make-new", function() {
-    // AJAX POST call to the submit route on the server
-    // This will take the data from the form and send it to the server
+  // Whenever someone clicks a p tag
+  $(document).on("click", "p", function() {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
     $.ajax({
-      type: "POST",
-      dataType: "json",
-      url: "/submit",
-      data: {
-        title: $("#title").val(),
-        note: $("#note").val(),
-        created: Date.now()
-      }
+      method: "GET",
+      url: "/articles/" + thisId
     })
-    // If that API call succeeds, add the title and a delete button for the note to the page
+      // With that done, add the note information to the page
       .then(function(data) {
-      // Add the title and delete button to the #results section
-        $("#results").prepend("<p class='data-entry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
-        data._id + ">" + data.title + "</span><span class=delete>X</span></p>");
-        // Clear the note and title inputs on the page
-        $("#note").val("");
-        $("#title").val("");
+        console.log(data);
+        // The title of the article
+        $("#notes").append("<h2>" + data.title + "</h2>");
+        // An input to enter a new title
+        $("#notes").append("<input id='titleinput' name='title' >");
+        // A textarea to add a new note body
+        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        // A button to submit a new note, with the id of the article saved to it
+        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+  
+        // If there's a note in the article
+        if (data.note) {
+          // Place the title of the note in the title input
+          $("#titleinput").val(data.note.title);
+          // Place the body of the note in the body textarea
+          $("#bodyinput").val(data.note.body);
+        }
       });
   });
   
-  // When the #clear-all button is pressed
-  $("#clear-all").on("click", function() {
-    // Make an AJAX GET request to delete the notes from the db
+  // When you click the savenote button
+  $(document).on("click", "#savenote", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
     $.ajax({
-      type: "GET",
-      dataType: "json",
-      url: "/clearall",
-      // On a successful call, clear the #results section
-      success: function(response) {
-        $("#results").empty();
-      }
-    });
-  });
-  
-  
-  // When user clicks the delete button for a note
-  $(document).on("click", ".delete", function() {
-    // Save the p tag that encloses the button
-    var selected = $(this).parent();
-    // Make an AJAX GET request to delete the specific note
-    // this uses the data-id of the p-tag, which is linked to the specific note
-    $.ajax({
-      type: "GET",
-      url: "/delete/" + selected.attr("data-id"),
-  
-      // On successful call
-      success: function(response) {
-        // Remove the p-tag from the DOM
-        selected.remove();
-        // Clear the note and title inputs
-        $("#note").val("");
-        $("#title").val("");
-        // Make sure the #action-button is submit (in case it's update)
-        $("#action-button").html("<button id='make-new'>Submit</button>");
-      }
-    });
-  });
-  
-  // When user click's on note title, show the note, and allow for updates
-  $(document).on("click", ".dataTitle", function() {
-    // Grab the element
-    var selected = $(this);
-    // Make an ajax call to find the note
-    // This uses the data-id of the p-tag, which is linked to the specific note
-    $.ajax({
-      type: "GET",
-      url: "/find/" + selected.attr("data-id"),
-      success: function(data) {
-        // Fill the inputs with the data that the ajax call collected
-        $("#note").val(data.note);
-        $("#title").val(data.title);
-        // Make the #action-button an update button, so user can
-        // Update the note s/he chooses
-        $("#action-button").html("<button id='updater' data-id='" + data._id + "'>Update</button>");
-      }
-    });
-  });
-  
-  // When user click's update button, update the specific note
-  $(document).on("click", "#updater", function() {
-    // Save the selected element
-    var selected = $(this);
-    // Make an AJAX POST request
-    // This uses the data-id of the update button,
-    // which is linked to the specific note title
-    // that the user clicked before
-    $.ajax({
-      type: "POST",
-      url: "/update/" + selected.attr("data-id"),
-      dataType: "json",
+      method: "POST",
+      url: "/articles/" + thisId,
       data: {
-        title: $("#title").val(),
-        note: $("#note").val()
-      },
-      // On successful call
-      success: function(data) {
-        // Clear the inputs
-        $("#note").val("");
-        $("#title").val("");
-        // Revert action button to submit
-        $("#action-button").html("<button id='make-new'>Submit</button>");
-        // Grab the results from the db again, to populate the DOM
-        getResults();
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
       }
-    });
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
   });
   
--- server.js
+++ server.js
@@ -1,186 +1,124 @@
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
 var logger = require("morgan");
-var path = require("path");
+var mongoose = require("mongoose");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-mongoose.connect(MONGODB_URI);
+// Require all models
+var db = require("./models");
 
+var PORT = 3000;
 
+// Initialize Express
 var app = express();
 
-// Set the app up with morgan.
-// morgan is used to log our HTTP Requests. By setting morgan to 'dev'
-// the :status token will be colored red for server error codes,
-// yellow for client error codes, cyan for redirection codes,
-// and uncolored for all other codes.
-app.use(logger("dev"));
+// Configure middleware
 
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Database configuration
-var databaseUrl = "notetaker";
-var collections = ["notes"];
-
-// Hook mongojs config to db variable
-var db = mongojs(databaseUrl, collections);
-
-// Log any mongojs errors to console
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // Routes
-// ======
 
-// Simple index route
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-// Handle form submission, save submission to mongo
-app.post("/submit", function(req, res) {
-  console.log(req.body);
-  // Insert the note into the notes collection
-  db.notes.insert(req.body, function(error, saved) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    else {
-      // Otherwise, send the note back to the browser
-      // This will fire off the success function of the ajax request
-      res.send(saved);
-    }
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
   });
 });
 
-// Retrieve results from mongo
-app.get("/all", function(req, res) {
-  // Find all notes in the notes collection
-  db.notes.find({}, function(error, found) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    else {
-      // Otherwise, send json of the notes back to user
-      // This will fire off the success function of the ajax request
-      res.json(found);
-    }
-  });
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
-// Select just one note by an id
-app.get("/find/:id", function(req, res) {
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IdYouWantToFind))
-
-  // Find just one result in the notes collection
-  db.notes.findOne(
-    {
-      // Using the id in the url
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    function(error, found) {
-      // log any errors
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the note to the browser
-        // This will fire off the success function of the ajax request
-        console.log(found);
-        res.send(found);
-      }
-    }
-  );
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
-// Update just one note by an id
-app.post("/update/:id", function(req, res) {
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IdYouWantToFind))
-
-  // Update the note that matches the object id
-  db.notes.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      // Set the title, note and modified parameters
-      // sent in the req body.
-      $set: {
-        title: req.body.title,
-        note: req.body.note,
-        modified: Date.now()
-      }
-    },
-    function(error, edited) {
-      // Log any errors from mongojs
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the mongojs response to the browser
-        // This will fire off the success function of the ajax request
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
-// Delete One from the DB
-app.get("/delete/:id", function(req, res) {
-  // Remove a note using the objectID
-  db.notes.remove(
-    {
-      _id: mongojs.ObjectID(req.params.id)
-    },
-    function(error, removed) {
-      // Log any errors from mongojs
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
-        // Otherwise, send the mongojs response to the browser
-        // This will fire off the success function of the ajax request
-        console.log(removed);
-        res.send(removed);
-      }
-    }
-  );
-});
-
-// Clear the DB
-app.get("/clearall", function(req, res) {
-  // Remove every note from the notes collection
-  db.notes.remove({}, function(error, response) {
-    // Log any errors to the console
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    else {
-      // Otherwise, send the mongojs response to the browser
-      // This will fire off the success function of the ajax request
-      console.log(response);
-      res.send(response);
-    }
-  });
-});
-
-// Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
