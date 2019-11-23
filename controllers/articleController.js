d05689379be7d4b56293d2343b9b676df00f0d01
Toney Higgins <toneyhiggins2@gmail.com>
2019-11-23 15:15:07 -0500

    adding handlebars

---
+++ controllers/articleController.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var db = require("../models");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+  db.Article.find({}).then((data) => {
+    res.render("index", { article: data });
+  });
+});
+
+
+// A GET route for scraping the /r/LiverpoolFC
+router.get("/scrape", (req, res) => {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.cnet.com/news/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every div with a top-matter class, and do the following:
+    $(".riverPost").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // filter on just media links
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).parent().children("p").children("a").text();
+      result.link = $(this).parent().children("p").children("a").attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (article) {
+          // View the added result in the console
+          console.log(article)
+        })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.status(200).send("Scrape Complete");
+  });
+});
+
+router.get("/comments/:id", (req, res) => {
+
+  let articleId = req.params.id;
+  console.log(articleId);
+  db.Article.findById(articleId)
+    .populate("comment")
+    .then((article) => {
+      console.log(article.comment)
+      res.json(article.comment)
+    })
+});
+
+
+router.post("/comments/:id", (req, res) => {
+
+  console.log(req.body)
+
+  db.Comment.create(req.body)
+    .then((comment) => {
+      return db.Article.findOneAndUpdate({_id: req.params.id}, {$push: { comment: comment._id }} , {new: true});
+    })
+    .then((added)=> res.status(200).send(`Comment added: ${added}`))
+    .catch(err => res.status(500).send(err))
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
--- models/Article.js
+++ models/Article.js
@@ -19,9 +19,9 @@ var ArticleSchema = new Schema({
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: {
+  comment: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: "Comment"
   }
 });
 
